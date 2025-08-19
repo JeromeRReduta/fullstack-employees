74e7ac8ccdc9ed430daae3af662a0aac7a868c10
JRReduta <jrreduta@yahoo.com>
2025-08-18 18:02:13 -0700

    Completed api functionality

--- api/employees.js
+++ api/employees.js
@@ -1,5 +1,92 @@
+import {
+  createEmployee,
+  deleteEmployee,
+  getEmployee,
+  getEmployees,
+  updateEmployee,
+} from "#db/queries/employees";
 import express from "express";
 const router = express.Router();
-export default router;
 
-// TODO: this file!
+router
+  .route("/")
+  .get(async (req, res) => {
+    const employees = await getEmployees();
+    return res.status(200).send(employees);
+  })
+  .post(async (req, res) => {
+    if (!req.body) {
+      return res.status(400).send("Body not provided");
+    }
+    const isBodyInvalid =
+      !req.body.name || !req.body.birthday || !req.body.salary;
+    if (isBodyInvalid) {
+      return res.status(400).send("Missing required field");
+    }
+    const newEmployee = await createEmployee({
+      name: req.body.name,
+      birthday: req.body.birthday,
+      salary: req.body.salary,
+    });
+    return res.status(201).send(newEmployee);
+  });
+
+router
+  .route("/:id")
+  .get(async (req, res) => {
+    const MAX_INT_SQL = 2147483647;
+    const id = +req.params.id;
+    console.log("id is", id);
+    console.log("larger than max?", id > MAX_INT_SQL);
+    if (isNaN(id) || id <= 0 || id > MAX_INT_SQL || !Number.isInteger(id)) {
+      return res.status(400).send("Not positive integer!");
+    }
+    const employee = await getEmployee(id);
+    if (!employee) {
+      return res.status(404).send("No employee found!");
+    }
+    return res.status(200).send(employee);
+  })
+  .put(async (req, res) => {
+    if (!req.body) {
+      return res.status(400).send("No body!");
+    }
+    const isBodyInvalid =
+      !req.body.name || !req.body.birthday || !req.body.salary;
+    if (isBodyInvalid) {
+      return res.status(400).send("Missing required field");
+    }
+    const MAX_INT_SQL = 2147483647;
+    const id = +req.params.id;
+    console.log("id is", id);
+    console.log("larger than max?", id > MAX_INT_SQL);
+    if (isNaN(id) || id <= 0 || id > MAX_INT_SQL || !Number.isInteger(id)) {
+      return res.status(400).send("Not positive integer!");
+    }
+    const updatedEmployee = await updateEmployee({
+      id: id,
+      name: req.body.name,
+      birthday: req.body.birthday,
+      salary: req.body.salary,
+    });
+    if (!updatedEmployee) {
+      return res.status(404).send("No employee found to update");
+    }
+    return res.status(200).send(updatedEmployee);
+  })
+  .delete(async (req, res) => {
+    const MAX_INT_SQL = 2147483647;
+    const id = +req.params.id;
+    console.log("id is", id);
+    console.log("larger than max?", id > MAX_INT_SQL);
+    if (isNaN(id) || id <= 0 || id > MAX_INT_SQL || !Number.isInteger(id)) {
+      return res.status(400).send("Not positive integer!");
+    }
+    const deletedEmployee = await deleteEmployee(id);
+    if (!deletedEmployee) {
+      return res.status(404).send("No employee found!");
+    }
+    return res.status(204).send();
+  });
+
+export default router;
--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 import express from "express";
+import employeeRouter from "#api/employees";
 const app = express();
-export default app;
+app.use(express.json());
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Broken dang");
+});
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to the Fullstack Employees API.");
+});
+app.use("/employees", employeeRouter);
 
 // TODO: this file!
+
+export default app;
